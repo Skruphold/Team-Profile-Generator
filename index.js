d04537ef72ed05684749833e0c9f8f13fe2cf7a5
Skruphold <hunterco999@gmail.com>
2022-01-19 17:04:15 -0600

    added logic for the intern and manager tests started the inquirer prompt on my index.js

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ const pageTemplate = require('./src/page-template');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            message: 'Name of the team manager?',
+            name: 'name',
+            validate: nameEntered => {
+                if (nameEntered) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the manager');
+                    return false;
+                }
+            }
+        }
+    ])
+} 
+
+addManager()

cc3f88af39a1a8d3e9ab1d793314529f24fe4b2f
Skruphold <hunterco999@gmail.com>
2022-01-19 20:22:47 -0600

    added the add employee function working

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const employeeArray = []
+
 const addManager = () => {
     return inquirer.prompt ([
         {
@@ -16,18 +18,18 @@ const addManager = () => {
                 if (nameEntered) {
                     return true;
                 } else {
-                    console.log('Please enter the name of the manager');
+                    console.log('Please enter the name of the manager.');
                     return false;
                 }
             }
         },
         {
             type: 'input',
-            message: "What is your manager's ID",
+            message: "What is your manager's ID?",
             name: 'id',
             validate: idNum => {
                 if (isNaN(idNum)) {
-                    console.log("please enter the manager's id");
+                    console.log("please enter the manager's id.");
                     return false;
                 } else {
                     return true;
@@ -43,7 +45,7 @@ const addManager = () => {
                 if (valid) {
                     return true;
                 } else {
-                    console.log("Please enter the manager's email");
+                    console.log("Please enter the manager's email.");
                     return false;
                 }
             }
@@ -54,7 +56,7 @@ const addManager = () => {
             name: 'officeNumber',
             validate: officeNum => {
                 if (isNaN(officeNum)) {
-                    console.log("please enter the manager's office number");
+                    console.log("please enter the manager's office number.");
                     return false;
                 } else {
                     return true;
@@ -63,6 +65,98 @@ const addManager = () => {
         }
 
     ])
-} 
+    .then(managerData => {
+        const { name, id, email, officeNumber } = managerData
+        const manager = new Manager(name, id, email, officeNumber);
+        console.log(manager);
+        employeeArray.push(manager);
+    })
+};
 
-addManager()
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            message: 'Please choose the position of the employee.',
+            name: 'position',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            message: "What is the employee's name?",
+            name: 'name',
+            validate: nameEntered => {
+                if (nameEntered) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the employee.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the employee's ID?",
+            name: 'id',
+            validate: idNum => {
+                if (isNaN(idNum)) {
+                    console.log('Please enter the ID for the employee.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the employee's email?",
+            name: 'email',
+            validate: eMail => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's email.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's github username?",
+            name: 'github',
+            when: (input) => input.position === "Engineer",
+            validate: nameEntered => {
+                if (nameEntered) {
+                    return true;
+                } else {
+                    console.log('Please enter the github username for the engineer.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What school is the intern coming from?",
+            name: 'school',
+            when: (input) => input.position === "Intern",
+            validate: nameEntered => {
+                if (nameEntered) {
+                    return true;
+                } else {
+                    console.log('Please enter the school that the intern came from.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add another employee?',
+            name: 'confirmNewEmployee'
+        }
+    ])
+}
+
+
+// addManager();
+addEmployee()
